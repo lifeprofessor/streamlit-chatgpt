5c8294e8667638d668675ef80bf5c31b16206b4d
lifeprofessor <116335791+lifeprofessor@users.noreply.github.com>
2023-11-10 14:14:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from openai import OpenAI
+# from langchain.llms import OpenAI
+import os
 import streamlit as st
-
-# from dotenv import load_dotenv
-# load_dotenv()
 
 # ChatGPT 사용법
 st.write('### :orange[OpenAI API - ChatGPT 사용법]')
 st.write('* MBTI 유형별 맞춤 공부법을 추천해주는 AI입니다.')
 
+os.environ["OPENAI_API_KEY"] = "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"
+
+
 client = OpenAI(
-    # api_key="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"
+    api_key="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"
 )
 
-question = st.text_input('본인의 MBTI를 적어주세요.', "")
+col1, col2 = st.columns(2)
+with col1:
+    question = st.text_input('본인의 MBTI를 적어주세요.', "")
+with col2:
+    st.write(' ')
+    st.link_button("MBTI 유형 검사하러 가기", "https://www.16personalities.com/ko/%EB%AC%B4%EB%A3%8C-%EC%84%B1%EA%B2%A9-%EC%9C%A0%ED%98%95-%EA%B2%80%EC%82%AC")
+
 gptModel = st.selectbox(
    "어떤 GPT 모델에게 답변 받고 싶은가요?",
    ("gpt-3.5-turbo", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613"),
